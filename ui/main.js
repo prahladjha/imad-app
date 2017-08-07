6e92afa7c03a16de9a037c386694ff3d64b78fb0
prahladjha <prahladjhakumar@gmail.com>
2017-08-07 13:37:12 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+//counnter side
+var button=document.getElementById("counter");
+var counter;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+}
