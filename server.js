d32456cef3b61ea6efc59ce650bf796c52de0c1c
prahladjha <prahladjhakumar@gmail.com>
2017-08-07 13:24:45 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article1',function(req,res)
+{
+  res.send("Article one requested");  
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
